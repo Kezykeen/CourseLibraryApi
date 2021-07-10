bfdf859db21d4f2f1f11432641a4ae38306b4646
kezyApps <k.okoro@genesystechhub.com>
2021-07-10 15:00:23 +0100

    Implemented Cross-property validation

---
+++ ValidationAttributes/EnsureTitleAndDescriptionAreDifferent.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CourseLibraryApi.Models;
+
+namespace CourseLibraryApi.ValidationAttributes
+{
+    public class EnsureTitleAndDescriptionAreDifferent : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var courseForCreationDto = validationContext.ObjectInstance as CourseForManipulationDto;
+
+            return courseForCreationDto.Title == courseForCreationDto.Description ? 
+                new ValidationResult(ErrorMessage, new []{nameof(CourseForCreationDto) }) 
+                : ValidationResult.Success;
+        }
+    }
+}
