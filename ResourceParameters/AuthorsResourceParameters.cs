e31fef2813b355191fe6dcbbbf8979b93b4f72ea
kezyApps <k.okoro@genesystechhub.com>
2021-07-10 14:59:38 +0100

    Added AuthorsResourceParameters for query

---
+++ ResourceParameters/AuthorsResourceParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace CourseLibraryApi.ResourceParameters
+{
+    public class AuthorsResourceParameters
+    {
+        private const int MaxPageSize = 20;
+
+        private int _defaultPageSize = 10;
+
+        public string MainCategory { get; set; }
+        public string SearchTerm { get; set; }
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize
+        {
+            get => _defaultPageSize;
+            set => _defaultPageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
+    }
+}
