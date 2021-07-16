21e3c69d5ce61796c308f2b3c1cce199f88b334b
kezyApps <k.okoro@genesystechhub.com>
2021-07-16 14:33:16 +0100

    Implemented iqueryable extension for data sorting

---
+++ Helpers/IQueryableExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using CourseLibraryApi.Services;
+
+namespace CourseLibraryApi.Helpers
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> ApplySort<T>(this IQueryable<T> source, string orderBy,
+            Dictionary<string, PropertyMappingValue> mappingDictionary)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (mappingDictionary == null)
+            {
+                throw new ArgumentNullException(nameof(mappingDictionary));
+            }
+
+            if (string.IsNullOrWhiteSpace(orderBy))
+            {
+                return source;
+            }
+
+            var orderByString = string.Empty;
+
+            // the orderBy string is separated by "," so we split it.
+            var orderByAfterSplit = orderBy.Split(",");
+
+            // apply each orderBy clause in reverse order - otherwise, the 
+            // IQueryable will be ordered in the wrong order
+            foreach (var orderByClause in orderByAfterSplit.Reverse())
+            {
+                var trimmedOrderByClause = orderByClause.Trim();
+
+                // if the sort option ends with "desc", we order by
+                // descending, otherwise ascending
+                var orderDescending = trimmedOrderByClause.EndsWith(" desc");
+
+                // remove " asc" or " desc" from the orderBy clause, so we get 
+                // the property name to look for in the mapping dictionary
+                var indexOfFirstSpace = trimmedOrderByClause.IndexOf(" ");
+                var propertyName = indexOfFirstSpace == -1
+                    ? trimmedOrderByClause
+                    : trimmedOrderByClause.Remove(indexOfFirstSpace);
+
+                // find the matching property
+                if (!mappingDictionary.ContainsKey(propertyName))
+                {
+                    throw new ArgumentException($"Key mapping for {propertyName} is missing");
+                }
+
+                // get the propertyMappingValue
+                var propertyMappingValue = mappingDictionary[propertyName];
+
+                if (propertyMappingValue == null)
+                {
+                    throw new ArgumentNullException(nameof(propertyMappingValue));
+                }
+
+                // revert sort order if necessary
+                if (propertyMappingValue.Revert)
+                {
+                    orderDescending = !orderDescending;
+                }
+
+                // Run through the property names
+                // so the orderby clauses are applied in the correct order
+                foreach (var destinationProperty in propertyMappingValue.DestinationProperties)
+                {
+                    orderByString = orderByString 
+                                    + (string.IsNullOrWhiteSpace(orderByString) ? string.Empty : ", ") 
+                                    + destinationProperty 
+                                    + (orderDescending ? " descending" : " ascending");
+                }
+            }
+
+            return source.OrderBy(orderByString);
+        }
+    }
+}
