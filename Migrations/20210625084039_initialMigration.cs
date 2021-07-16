bbe25b43a323bf7c8356883fa5a3b64c249f85fc
kezyApps <k.okoro@genesystechhub.com>
2021-07-16 14:25:16 +0100

    Added Migrations

--- Migrations/20210625084039_initialMigration.cs
+++ Migrations/20210625084039_initialMigration.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace LibraryCourseApi.Migrations
+namespace CourseLibraryApi.Migrations
 {
     public partial class InitialMigration : Migration
     {
